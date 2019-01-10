a0039abfd83b46c1852271d071004080dc00f1e7
Luis Henrique Souza <mora0199@algonquinlive.com>
2019-01-09 19:49:51 -0500

    adds

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let canvas, context;
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+
+    canvas.width = 600;
+    canvas.height = 400;
+})
