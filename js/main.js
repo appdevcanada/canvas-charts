31fe010bda57e3763b933ba1f3c4911d112f73d3
Luis Henrique Souza <mora0199@algonquinlive.com>
2019-01-10 14:22:50 -0500

    chart1

--- js/main.js
+++ js/main.js
@@ -6,4 +6,15 @@ document.addEventListener('DOMContentLoaded', (e)=>{
 
     canvas.width = 600;
     canvas.height = 400;
-})
+    
+    context.strokeStyle = "black";
+    context.fillStyle = "gold";
+    context.beginPath();
+    context.moveTo(200, 200);
+    let radius = 100;
+    context.arc(300,200,radius, 0, Math.PI/3);
+    context.lineWidth = 2;
+    context.stroke();
+    context.fill();
+    context.closePath();
+});
