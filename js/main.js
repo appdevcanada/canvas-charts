10ecc71e8e77443d520e002d5b33c66bbe7751e0
Luis Henrique Souza <mora0199@algonquinlive.com>
2019-01-11 14:34:14 -0500

    initial chart

--- js/main.js
+++ js/main.js
@@ -12,9 +12,19 @@ document.addEventListener('DOMContentLoaded', (e)=>{
     context.beginPath();
     context.moveTo(200, 200);
     let radius = 100;
-    context.arc(300,200,radius, 0, Math.PI/3);
-    context.lineWidth = 2;
+    context.arc(300,200,radius, 0, Math.PI+1);
+    context.lineWidth = 1;
+    context.lineTo(300,200);
+    context.font = '1rem Calibri, Helvetica, Arial, sans-serif';
+    context.textAlign = 'start';  //same as left if text direction is ltr
+    //context.fillStyle = "black";
+    context.fillText(calcPerc(Math.PI+1), 100, 20);
     context.stroke();
     context.fill();
     context.closePath();
 });
+
+function calcPerc(valToPerc) {
+    let retPerc = Math.round(valToPerc * 100 / (2*Math.PI));
+    return retPerc;
+}
